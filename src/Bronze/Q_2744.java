e5670df43fc794fdc5701845ddabc0a7ea8c841f
lKori <yuy0841@gmail.com>
2023-05-14 03:08:18 +0900

    20230513

--- src/Bronze/Q_2744.java
+++ src/Bronze/Q_2744.java
@@ -1,13 +1,10 @@
 package Bronze;
 
 import java.io.*;
-import java.util.StringTokenizer;
 
 public class Q_2744 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
 
         String s = br.readLine();
         StringBuilder sb = new StringBuilder();

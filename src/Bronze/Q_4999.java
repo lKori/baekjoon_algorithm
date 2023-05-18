0dde8186fdba6deaae721b7273d5b79bd18d60cc
lKori <yuy0841@gmail.com>
2023-05-18 21:58:15 +0900

    20230518

---
+++ src/Bronze/Q_4999.java
@@ -0,0 +1,33 @@
+package Bronze;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Q_4999 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String s1 = br.readLine();
+        int countA1 = 0;
+
+        for(int i = 0; i < s1.length(); i++) {
+            if(s1.charAt(i) == 'a') {
+                countA1++;
+            }
+        }
+
+        String s2 = br.readLine();
+        int countA2 = 0;
+
+        for(int j = 0; j < s2.length(); j++) {
+            if(s2.charAt(j) == 'a') {
+                countA2++;
+            }
+        }
+
+        System.out.print(countA1 >= countA2 ? "go" : "no");
+    }
+}

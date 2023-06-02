ab94d9fb3919ecd64c8b25e2431ae7476c934cc7
lKori <yuy0841@gmail.com>
2023-06-03 00:00:31 +0900

    20230602

---
+++ src/Bronze/Q_5524.java
@@ -0,0 +1,21 @@
+package Bronze;
+
+import java.io.*;
+
+public class Q_5524 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+            s = s.toLowerCase();
+
+            bw.write(s + "\n");
+        }
+
+        bw.close();
+    }
+}

2af2bb95a55c2d35dc69adbe6d44ab0a11fe5164
lKori <yuy0841@gmail.com>
2023-07-09 20:06:36 +0900

    20230709

---
+++ src/Bronze/Q_15000.java
@@ -0,0 +1,13 @@
+package Bronze;
+
+import java.io.*;
+
+public class Q_15000 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println(s.toUpperCase());
+    }
+}
